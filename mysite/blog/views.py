9e7916364cb7b79ffb60a2444945296efbd3c1a6
Joshua <Joshua>
2017-05-13 18:17:12 +0900

    render 함수 이용. 블로그 첫 화면

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    post_all_data = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'post': post_all_data})
 
 
 
