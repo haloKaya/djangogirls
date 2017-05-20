d1cab483b2942f9605ff32a8cc3d3b4d7fafe0f9
Joshua <Joshua>
2017-05-20 14:09:12 +0900

    상세 페이지 url 만들기

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -9,4 +10,6 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'post': post_all_data})
 
 
-
+def post_detail(request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'blog/post_detail.html', {'post': post})
