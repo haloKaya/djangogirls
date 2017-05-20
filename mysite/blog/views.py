b4342d6ec7a90ff7cb017358cdbe66c6786898d2
Joshua <Joshua>
2017-05-20 18:22:59 +0900

    수정,추가 Url & DB

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,15 +1,47 @@
 # -*- coding: utf-8 -*-
+from django.utils import timezone
 from django.shortcuts import render
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 
 # Create your views here.
 def post_list(request):
     post_all_data = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'post': post_all_data})
+    return render(request, 'blog/post_list.html', {'posts': post_all_data})
 
 
 def post_detail(request, pk):
         post = get_object_or_404(Post, pk=pk)
         return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
